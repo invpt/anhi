d65b435d0f0b43c3db74b51b34196539bfd7df7c
sl4b <57822954+sl4b@users.noreply.github.com>
2022-03-17 03:57:23 -0400

    Work on looks of overview page

--- lib/screens/overview/overview_card.dart
+++ lib/screens/overview/overview_card.dart
@@ -18,20 +18,39 @@ class OverviewCard extends StatelessWidget {
     return SizedBox(
       width: double.infinity,
       child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
         child: InkWell(
           onTap: () {},
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  secret.mnemonic,
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-                Text(
-                  "Review $available",
-                  style: Theme.of(context).textTheme.subtitle2,
+            padding: const EdgeInsets.only(
+              left: 12.0,
+              right: 8.0,
+              top: 8.0,
+              bottom: 8.0,
+            ),
+            child: Row(
+              children: [
+                Text("7",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        ?.copyWith(fontWeight: FontWeight.w300)),
+                const SizedBox(width: 12.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      secret.mnemonic,
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    Text(
+                      "Review $available",
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle2
+                          ?.copyWith(fontWeight: FontWeight.w300),
+                    ),
+                  ],
                 ),
               ],
             ),
