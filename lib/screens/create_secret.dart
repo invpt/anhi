f2f2707b0e32182aed7cf9de3369f40077dd0827
sl4b <57822954+sl4b@users.noreply.github.com>
2022-03-17 03:03:45 -0400

    Move create secret back to separate page

---
+++ lib/screens/create_secret.dart
@@ -0,0 +1,97 @@
+import 'package:anhi/secret.dart';
+import 'package:anhi/secret_storage.dart';
+import 'package:flutter/material.dart';
+
+class CreateSecretPage extends StatefulWidget {
+  const CreateSecretPage({Key? key, required this.storage}) : super(key: key);
+
+  final SecretStorage storage;
+
+  @override
+  State<StatefulWidget> createState() => _CreateSecretPageState();
+}
+
+class _CreateSecretPageState extends State<CreateSecretPage> {
+  String? mnemonicError;
+  String mnemonic = '';
+  String value = '';
+
+  void finish(BuildContext context, {required bool save}) {
+    if (!save) {
+      Navigator.pop(context);
+    } else if (checkError()) {
+      Secret.newSecret(mnemonic, value)
+          .then((secret) => Navigator.pop(context, secret));
+    }
+  }
+
+  bool checkError() {
+    final isValid = mnemonic.isNotEmpty && !widget.storage.exists(mnemonic);
+
+    // set the error message
+    setState(() {
+      if (isValid) {
+        mnemonicError = null;
+      } else if (mnemonic.isEmpty) {
+        mnemonicError = 'You must enter a mnemonic.';
+      } else {
+        mnemonicError = 'Another secret with that mnemonic already exists.';
+      }
+    });
+
+    return isValid;
+  }
+
+  void updateMnemonic(String mnemonic) {
+    setState(() => this.mnemonic = mnemonic);
+    checkError();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create secret'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              autofocus: true,
+              autocorrect: true,
+              onChanged: updateMnemonic,
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                labelText: 'Mnemonic',
+                hintText: 'Enter a mnemonic',
+                errorText: mnemonicError,
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(8.0)),
+            TextField(
+              obscureText: true,
+              textInputAction: TextInputAction.done,
+              onChanged: (newSecret) {
+                value = newSecret;
+              },
+              onSubmitted: (_) => finish(context, save: true),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Secret',
+                hintText: 'Enter a secret',
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => finish(context, save: true),
+        tooltip: 'Done',
+        child: const Icon(Icons.done),
+      ),
+    );
+  }
+}
--- lib/screens/overview.dart
+++ lib/screens/overview.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
-
-import 'package:anhi/screens/overview/create_secret_card.dart';
+import 'package:anhi/screens/create_secret.dart';
 import 'package:anhi/screens/review.dart';
+import 'package:anhi/secret.dart';
 import 'package:anhi/secret_storage.dart';
 import 'package:flutter/material.dart';
 
-import '../secret.dart';
 import 'overview/overview_card.dart';
 
 class OverviewPage extends StatefulWidget {
@@ -21,34 +19,13 @@ class _OverviewPageState extends State<OverviewPage> {
     onError: (e) => throw e,
   );
 
-  final createSecretController = CreateSecretCardController();
-  bool isNewSecretVisible = false;
-
-  void onCreateSecretDone(Secret? secret) {
-    if (secret != null) {
-      setState(() => _storage.add(secret));
-    }
-
-    setState(() => isNewSecretVisible = false);
-  }
-
   void pushReviewPage() {
     Navigator.push(context, MaterialPageRoute<void>(builder: (context) {
       return ReviewPage(
         reviews: reviewableSecrets(),
         storage: _storage,
       );
-    })).then((_) =>
-        // Update state, since the review page probably updated secrets
-        setState(() {}));
-  }
-
-  void hideNewSecret({required bool save}) {
-    createSecretController.trySubmit(save: save);
-  }
-
-  void showNewSecret() {
-    setState(() => isNewSecretVisible = true);
+    })).then((_) => setState(() {}));
   }
 
   List<StoredSecret> reviewableSecrets() {
@@ -57,111 +34,49 @@ class _OverviewPageState extends State<OverviewPage> {
         .toList();
   }
 
-  // This handles back button presses on Android
-  Future<bool> _onWillPop() async {
-    if (isNewSecretVisible) {
-      hideNewSecret(save: false);
-
-      return false;
-    } else {
-      return true;
-    }
+  void pushCreateSecretPage() {
+    Navigator.push(context, MaterialPageRoute<Secret?>(builder: (context) {
+      return CreateSecretPage(
+        storage: _storage,
+      );
+    })).then((secret) {
+      if (secret != null) {
+        setState(() => _storage.add(secret));
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _onWillPop,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Anhi'),
-        ),
-        body: Column(
-          children: [
-            AnimatedSize(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.ease,
-              child: SizedBox(
-                height: isNewSecretVisible ? null : 0,
-                child: CreateSecretCard(
-                  onDone: onCreateSecretDone,
-                  controller: createSecretController,
-                  secretExists: _storage.exists,
-                ),
-              ),
-            ),
-            OverviewList(
-              isAside: isNewSecretVisible,
-              secrets: _storage.storedSecrets,
-            ),
-          ],
-        ),
-        floatingActionButton: Wrap(
-          direction: Axis.vertical,
-          children: [
-            Visibility(
-              visible: !isNewSecretVisible,
-              child: FloatingActionButton(
-                heroTag: null,
-                onPressed: pushReviewPage,
-                tooltip: 'Review secrets',
-                child: const Icon(Icons.book),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(4.0)),
-            FloatingActionButton(
-              heroTag: null,
-              onPressed: () {
-                if (isNewSecretVisible) {
-                  hideNewSecret(save: true);
-                } else {
-                  showNewSecret();
-                }
-              },
-              tooltip: 'Add secret',
-              child: isNewSecretVisible
-                  ? const Icon(Icons.done)
-                  : const Icon(Icons.add),
-            ),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Anhi'),
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(4.0),
+        itemCount: _storage.storedSecrets.length,
+        itemBuilder: (context, index) => OverviewCard(
+          _storage.storedSecrets[index],
+          key: ValueKey(_storage.storedSecrets[index].localId),
         ),
       ),
-    );
-  }
-}
-
-class OverviewList extends StatelessWidget {
-  const OverviewList({
-    Key? key,
-    required this.isAside,
-    required this.secrets,
-  }) : super(key: key);
-
-  final bool isAside;
-  final List<StoredSecret> secrets;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Opacity(
-        opacity: isAside ? 0.5 : 1.0,
-        child: IgnorePointer(
-          ignoring: isAside,
-          child: ListView.builder(
-            shrinkWrap: true,
-            padding: EdgeInsets.only(
-              left: 4.0,
-              right: 4.0,
-              bottom: 4.0,
-              top: isAside ? 0.0 : 4.0,
-            ),
-            itemCount: secrets.length,
-            itemBuilder: (context, index) => OverviewCard(
-              secrets[index],
-              key: ValueKey(secrets[index].localId),
-            ),
+      floatingActionButton: Wrap(
+        direction: Axis.vertical,
+        children: [
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: pushReviewPage,
+            tooltip: 'Review secrets',
+            child: const Icon(Icons.book),
           ),
-        ),
+          const Padding(padding: EdgeInsets.all(4.0)),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: pushCreateSecretPage,
+            tooltip: 'Create a secret',
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
